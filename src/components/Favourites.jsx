b05700f0c607a479cc928d76fdbc19aadf12d25b
Francisco-Fortes <fortesfco@gmail.com>
2022-12-01 17:01:55 +0100

    Favourites update
    Starting to implement useSelector & useDispatch

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,6 +1,9 @@
 import { Container, Row, Col, ListGroup, Button } from "react-bootstrap";
+import { useSelector, useDispatch } from "react-redux";
 
 const Favourites = () => {
+  useSelector((state) => state.favourites.content);
+
   return (
     <Container className="mt-5">
       <Row className="justify-content-center">
--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,33 +1,48 @@
 import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: "1px solid #00000033", borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={9}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-    <Button variant="none" style={{ width: "16px", height: "16px" }}>
-      <svg
-        style={{ color: "red" }}
-        xmlns="http://www.w3.org/2000/svg"
-        width="16"
-        height="16"
-        fill="currentColor"
-        class="bi bi-heart"
-        viewBox="0 0 16 16"
+const Job = ({ data }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={9}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Button
+        variant="none"
+        style={{ width: "16px", height: "16px" }}
+        onClick={() => {
+          //dispatch is always an action and an action is always a js object that contains at least type property
+          dispatch({
+            type: "ADD_TO_FAVOURITES",
+            payload: data,
+          });
+        }}
       >
-        <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-      </svg>
-    </Button>
-  </Row>
-);
+        <svg
+          style={{ color: "red" }}
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          class="bi bi-heart"
+          viewBox="0 0 16 16"
+        >
+          <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+        </svg>
+      </Button>
+    </Row>
+  );
+};
 
 export default Job;
